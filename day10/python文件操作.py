0d79b6d2e228e69e12e914c6120d162a68ae82db
lizhipeng1021 <lizhipeng1021@163.com>
2020-07-10 10:09:36 +0800

    updating

--- day10/python文件操作.py
+++ day10/python文件操作.py
@@ -226,3 +226,25 @@ def get_modify_time(indir):
             print('文件{}最后一次修改时间：{}'.format(file, nice_show_time))
 
 get_modify_time('./')
+
+'''
+批量压缩文件
+导入zipfile模块，压缩和解压的python文件
+'''
+import zipfile
+import time
+
+def batch_zip(start_dir):
+    start_dir = start_dir       #要压缩的文件夹路径
+    file_news = start_dir + '.zip'      #压缩后文件夹的名字
+
+    z = zipfile.ZipFile(file_news, 'w', zipfile.ZIP_DEFLATED)
+    for dir_path, dir_names, file_names in os.walk(start_dir):
+        #这一句很重要，不replace的话，就从根目录开始复制
+        f_path = dir_path.replace(start_dir, '')
+        f_path = f_path and f_path + os.sep     #实现当前文件夹以及包含的所有文件的压缩
+        for filename in file_names:
+            z.write(os.path.join(dir_path, filename), f_path + filename)
+        z.close()
+        return file_news
+batch_zip(r'E:\working\pythonAlgorithm\gitbook\day09')
